65fb29a7b08d71ccf086e68dcb5c9b297575e25d
Lukas Sopora <lukas@sopora.de>
2019-12-07 21:19:06 +0100

    add Student class and Gender Enum in Model

---
+++ src/Model/Gender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mvvm.Model
+{
+    enum Gender
+    {
+        Male,
+        Female,
+        Diverse
+    }
+}
---
+++ src/Model/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mvvm.Model
+{
+    class Student
+    {
+		private String m_Name;
+		private int m_Age;
+
+		public int Age
+		{
+			get { return m_Age; }
+			set { m_Age = value; }
+		}
+
+
+		public String Name
+		{
+			get { return m_Name; }
+			set { m_Name = value; }
+		}
+
+	}
+}
