5baef267c9c866fbf9445ad9f3df3e630d49f6a9
NicoVogel <nico-vogel97@outlook.de>
2019-12-08 13:22:54 +0000

    update Student to be simpler

--- csharp/src/Model/Student.cs
+++ csharp/src/Model/Student.cs
@@ -6,33 +6,15 @@ namespace mvvm.Model
 {
     public class Student
     {
-		private string m_Name;
-		private int m_Age;
-		private GenterType m_Gender;
+		public string Name { get; set; }
+		public int Age { get; set; }
+		public GenderType Gender { get; set; }
 
 		public Student(string name, int age, GenterType gender)
 		{
-			m_Name = name;
-			m_Age = age;
-			m_Gender = gender;
-		}
-
-		public string Name
-		{
-			get { return m_Name; }
-			set { m_Name = value; }
-		}
-
-		public int Age
-		{
-			get { return m_Age; }
-			set { m_Age = value; }
-		}
-
-		public GenterType Gender
-		{
-			get { return m_Gender; }
-			set { m_Gender = value; }
+			this.Name = name;
+			this.Age = age;
+			this.Gender = gender;
 		}
 	}
 }
