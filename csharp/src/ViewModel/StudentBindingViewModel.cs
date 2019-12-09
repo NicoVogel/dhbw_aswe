455df0ee16b096e709a59d82a130ffcbbd065391
Lukas Sopora <lukas@sopora.de>
2019-12-09 21:45:00 +0100

    implement some fancy shit for demonstrating two way binding with property chanded update sourcetrigger mode

--- csharp/src/ViewModel/StudentBindingViewModel.cs
+++ csharp/src/ViewModel/StudentBindingViewModel.cs
@@ -1,18 +1,43 @@
 ﻿using mvvm.Model;
+using mvvm.Utility;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace mvvm.ViewModel
 {
-    public class StudentBindingViewModel
+    public class StudentBindingViewModel : INotifyPropertyChanged
     {
         public StudentCollection ClassBook { get; }
-        public Student SelectedStudent { get; set; }
+        private Student m_SelectedStudent;
+
+        public Student SelectedStudent
+        {
+            get { return m_SelectedStudent; }
+            set 
+            {
+                if(m_SelectedStudent == value)
+                {
+                    return;
+                }
+                m_SelectedStudent = value;
+                OnPropertyChanged();
+            }
+        }
+
 
         public StudentBindingViewModel()
         {
-            ClassBook = new StudentCollection();
+            ClassBook = StudentTestDataUtility.GetStudentTestData();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
