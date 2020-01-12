9941533f6be9d3f228b6d217a850df5720808dbb
Lukas Sopora <lukas@sopora.de>
2020-01-12 23:49:11 +0100

    ilet Student inherit from Person

--- csharp/src/Model/Person.cs
+++ csharp/src/Model/Person.cs
@@ -4,7 +4,26 @@ using System.Text;
 
 namespace mvvm.Model
 {
-    class Person
+    public class Person
     {
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+        public GenderType Gender { get; set; }
+
+        public Person()
+        {
+        }
+
+        public Person(string name, DateTime birthday, GenderType gender)
+        {
+            this.Name = name;
+            this.Birthday = birthday;
+            this.Gender = gender;
+        }
+
+        public int GetAge()
+        {
+            return DateTime.Now.Year - this.Birthday.Year;
+        }
     }
 }
--- csharp/src/Model/Student.cs
+++ csharp/src/Model/Student.cs
@@ -1,15 +1,22 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace mvvm.Model
 {
-    public class Student
+    public class Student : Person
     {
-		public string Name { get; set; }
 		public int Age { get; set; }
-		public GenderType Gender { get; set; }
+		public StudentCollection ClassBook { get; set; }
+		public IList<double> Grades { get; set; }
+
+		public Student(string name, DateTime birthday, GenderType gender, StudentCollection classBook, List<double> grades) : base(name, birthday, gender)
+		{
+			this.ClassBook = classBook;
+			this.Grades = new List<double>(grades);
+		}
 
 		[JsonConstructor]
 		public Student(string name, int age, GenderType gender)
@@ -17,13 +24,27 @@ namespace mvvm.Model
 			this.Name = name;
 			this.Age = age;
 			this.Gender = gender;
+			this.Grades = new List<double>();
 		}
 
 		public Student(Student c_Student)
 		{
 			this.Name = c_Student.Name;
+			this.Birthday = c_Student.Birthday;
 			this.Age = c_Student.Age;
 			this.Gender = c_Student.Gender;
+			this.ClassBook = c_Student.ClassBook;
+			this.Grades = new List<double>(c_Student.Grades);
+		}
+
+		public double GetAverageGrade()
+		{
+			return this.Grades.Average();
+		}
+
+		public void AddGrade(double grade)
+		{
+			this.Grades.Add(grade);
 		}
 	}
 }
