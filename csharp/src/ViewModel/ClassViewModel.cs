fb76bd05859b4bac640ca4033668049c11ef991a
Lukas Sopora <lukas@sopora.de>
2020-01-19 16:40:19 +0100

    work on class view and viewmodel

--- csharp/src/ViewModel/ClassViewModel.cs
+++ csharp/src/ViewModel/ClassViewModel.cs
@@ -1,6 +1,37 @@
-﻿namespace mvvm.ViewModel
+﻿using mvvm.Model;
+using System.Collections.ObjectModel;
+
+namespace mvvm.ViewModel
 {
     public class ClassViewModel : ViewModelBase
     {
+        private ClassBook _classBook;
+        private ObservableCollection<Teacher> _teachers;
+
+        public ClassBook ClassBook
+        {
+            get
+            {
+                return _classBook;
+            }
+            set
+            {
+                _classBook = value;
+                OnPropertyChanged(nameof(ClassBook));
+            }
+        }
+
+        public ObservableCollection<Teacher> Teachers
+        {
+            get
+            {
+                return _teachers;
+            }
+            set
+            {
+                _teachers = value;
+                OnPropertyChanged(nameof(Teachers));
+            }
+        }
     }
 }
