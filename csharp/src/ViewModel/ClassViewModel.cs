3974b9d6861b08a25f45127f8046746beec4eef9
Lukas Sopora <lukas@sopora.de>
2020-01-24 20:42:23 +0100

    start with classviewmodel functionality implementation

--- csharp/src/ViewModel/ClassViewModel.cs
+++ csharp/src/ViewModel/ClassViewModel.cs
@@ -10,7 +10,57 @@ namespace mvvm.ViewModel
     {
         private ClassBook _classBook;
         private ObservableCollection<Teacher> _teachers;
+        private Student _selectedStudent;
 
+
+        public ICommand AddStudentCommand { get; private set; }
+        public ICommand OpenStudentCommand { get; private set; }
+        public ICommand DeleteStudentCommand { get; private set; }
+
+        public ClassViewModel()
+        {
+            InitializeCommands();
+        }
+
+        private void InitializeCommands()
+        {
+            AddStudentCommand = new DelegateCommand(OnAddStudent);
+            OpenStudentCommand = new DelegateCommand(OnOpenStudent);
+            DeleteStudentCommand = new DelegateCommand(OnDeleteStudent);
+        }
+
+        private void OnDeleteStudent()
+        {
+            if (SelectedStudent == null)
+            {
+                return;
+            }
+
+            ClassBook.Students.Remove(SelectedStudent);
+        }
+
+        private void OnOpenStudent()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnAddStudent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Student SelectedStudent
+        {
+            get 
+            {
+                return _selectedStudent; 
+            }
+            set
+            {
+                _selectedStudent = value;
+                OnPropertyChanged(nameof(SelectedStudent));
+            }
+        }
         public ClassBook ClassBook
         {
             get
@@ -21,7 +71,6 @@ namespace mvvm.ViewModel
             {
                 _classBook = value;
                 OnPropertyChanged(nameof(ClassBook));
-                OnPropertyChanged(nameof(ClassBook.Teacher));
             }
         }
 
