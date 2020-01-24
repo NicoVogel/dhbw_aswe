94ad2eb4dda21a174188be4f63eaf17920e28584
Lukas Sopora <lukas@sopora.de>
2020-01-24 23:59:11 +0100

    start with teacher view implementation

--- csharp/src/ViewModel/TeacherViewModel.cs
+++ csharp/src/ViewModel/TeacherViewModel.cs
@@ -1,11 +1,36 @@
-﻿using System;
+﻿using mvvm.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace mvvm.ViewModel
 {
     public class TeacherViewModel : ViewModelBase
     {
+		private Teacher _teacher;
+		private ObservableCollection<ClassBook> _classBooks;
+		public IList<ClassBook> AllClasses { get; set; } = new List<ClassBook>();
 
-    }
+		public ObservableCollection<ClassBook> ClassBooks
+		{
+			get { return _classBooks; }
+			set 
+			{
+				_classBooks = value;
+				OnPropertyChanged(nameof(ClassBooks));
+			}
+		}
+
+		public Teacher Teacher
+		{
+			get { return _teacher; }
+			set
+			{
+				_teacher = value;
+				OnPropertyChanged(nameof(Teacher));
+			}
+		}
+
+	}
 }
