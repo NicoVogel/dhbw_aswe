c14f387be9c91d9e76de9b06aa6d8c5b4d625150
Lukas Sopora (PEA4-Fe) <lukas.sopora@de.bosch.com>
2020-01-31 13:51:28 +0100

    work on documentation

---
+++ doc/code/MainContentSwitch.cs
@@ -0,0 +1,6 @@
+internal void OnTeacherView(Teacher selectedTeacher)
+{
+    TeacherViewModel.Teacher = selectedTeacher;
+    CurrentView = TeacherView;
+    OnPropertyChanged(nameof(CurrentView));
+}
